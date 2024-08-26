31f2a04511e13c1fa1cc7164dc1c9ef7ab76c048
jesse sheehan <jessetsheehan@gmail.com>
2024-08-26 09:49:46 -0400

    Starter Code

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Jesse Sheehan on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
