a4cd9f689346eb383f4e1dd3ecf54a70dcde5f3f
jesse sheehan <jessetsheehan@gmail.com>
2024-08-26 10:26:01 -0400

    Day 43- Hashable and NavigationDestination
    Most of swift's types already conform to hashable - you just need to specify new structs to be hashable.
    the navigationDestination() modifier tells NavigationLink what to do with the data you passed through it. It's like passing parameters into a function or method, except for Navigation Links and Views

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -7,15 +7,58 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//    }
+//}
+
+//FIRST PRACTICE
+
+struct DetailView: View {
+    var number: Int
+    
+    var body: some View {
+        Text("Detail View \(number)")
+    }
+    
+    init(number: Int) {
+        self.number = number
+        print("Creating detail view \(number)")
+    }
+}
+
+struct Student: Hashable {
+    var id = UUID()
+    var name: String
+    var age: Int
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+                //Navigation Links/the DetailView is created automatically, when opening the app.
+                //It's okay when there's just one, but not when there are tons - it'll make duplicates!
+                //DetailView(number: 556)
+                List(0..<100) {i in
+                  //  NavigationLink("Tap Me") {
+                       // DetailView(number: i) }
+                    NavigationLink("Select \(i)", value: i)
+                }
+                .navigationDestination(for: Int.self) { selection in
+                        Text("You selected \(selection)")
+                }
+                .navigationDestination(for: Student.self) { student in
+                    Text("You selected \(student.name)")
+                }
         }
-        .padding()
     }
 }
 
