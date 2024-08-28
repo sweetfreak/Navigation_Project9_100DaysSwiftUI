fbf1e6f97900ab04972830ec9a5db49c3465a633
jesse sheehan <jessetsheehan@gmail.com>
2024-08-28 14:50:12 -0400

    day 45 practice - navlink and editing the navigation bar

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -7,89 +7,130 @@
 
 import SwiftUI
 
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-//            Text("Hello, world!")
-//        }
-//        .padding()
-//    }
-//}
-
-@Observable
-class PathStore {
-    //NOTE: could use [Int] instead of navigationpaths and their codable representations, and it'll be simpler when you get to the encoding part
-    var path: NavigationPath {
-        didSet {
-            save()
-        }
-    }
-    
-    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
-    
-    init() {
-        if let data = try? Data(contentsOf: savePath) {
-            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
-                path = NavigationPath(decoded) //just "decode" if it's an [Int]
-                return
-            }
-        }
-        
-        path = NavigationPath()
-    }
-    
-    func save() {
-        
-        //makes sure the navigationPath is codable and can be used here.
-        guard let representation = path.codable else { return }
-        
-        do {
-            let data = try JSONEncoder().encode(representation)   //(path)
-            try data.write(to: savePath)
-        } catch {
-            print("Failed to save navigation data")
-        }
-    }
-    
-}
-
-
-struct DetailView: View {
-    var number: Int
-    //@Binding var path: [Int] // use NavigationPath instead?
-    @Binding var path: NavigationPath
-    
-    var body: some View {
-        NavigationLink("Go to random number", value: Int.random(in: 1...1000))
-            .navigationTitle("Number: \(number)")
-            .toolbar {
-                Button("Home") {
-                    path = NavigationPath()
-                }
-            }
-    }
-}
+//Day 45
 
 struct ContentView: View {
+    
+    @State private var title = "SwiftUI"
+    
+    var body: some View {
+        
+        NavigationStack {
+            Text("Hello, world")
+                .navigationTitle($title)
+                .navigationBarTitleDisplayMode(.inline)
+        }
+        
+//        NavigationStack {
+//            Text("Hello, world!")
+//                .toolbar {
+//                    ToolbarItemGroup(placement: .topBarLeading) {
+//                        Button("Tap Me") {
+//                            
+//                        
+//                        }
+//                        
+//                        Button("Or Tap Me") {
+//                            
+//                        }
+//                    }
+//                    
+//                    ToolbarItem(placement: .topBarLeading) {
+//                        Button("Or 3rd, Tap Me") {
+//
+//                        }
+//                    }
+//                    
+//                }
+//                .navigationBarBackButtonHidden() //can't go back till they made a choice - like cancel, save, etc
+//        }
+        
+//        NavigationStack {
+//            List(0..<100) { i in
+//                    Text("Row \(i)")
+//            }
+//            .navigationTitle("Title Goes Here")
+//            .navigationBarTitleDisplayMode(.inline)
+//            .toolbarBackground(.blue)// , for: .navigationBar)
+//            .toolbarColorScheme(.dark)
+//            //.toolbar(.hidden, for:.navigationBar)
+//        }
+    }
+}
+
+// Day 444
+
+//@Observable
+//class PathStore {
+//    //NOTE: could use [Int] instead of navigationpaths and their codable representations, and it'll be simpler when you get to the encoding part
+//    var path: NavigationPath {
+//        didSet {
+//            save()
+//        }
+//    }
+//    
+//    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+//    
+//    init() {
+//        if let data = try? Data(contentsOf: savePath) {
+//            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+//                path = NavigationPath(decoded) //just "decode" if it's an [Int]
+//                return
+//            }
+//        }
+//        
+//        path = NavigationPath()
+//    }
+//    
+//    func save() {
+//        
+//        //makes sure the navigationPath is codable and can be used here.
+//        guard let representation = path.codable else { return }
+//        
+//        do {
+//            let data = try JSONEncoder().encode(representation)   //(path)
+//            try data.write(to: savePath)
+//        } catch {
+//            print("Failed to save navigation data")
+//        }
+//    }
+//    
+//}
+//
+//
+//struct DetailView: View {
+//    var number: Int
+//    //@Binding var path: [Int] // use NavigationPath instead?
+//    @Binding var path: NavigationPath
+//    
+//    var body: some View {
+//        NavigationLink("Go to random number", value: Int.random(in: 1...1000))
+//            .navigationTitle("Number: \(number)")
+//            .toolbar {
+//                Button("Home") {
+//                    path = NavigationPath()
+//                }
+//            }
+//    }
+//}
+//
+//struct ContentView: View {
     
     //@State private var path = [Int]()
     
     //Navpath provide 2 helpers to help users save their path data
     //@State private var path = NavigationPath()
-    @State private var pathStore = PathStore()
-    
-    
-    var body: some View {
-        
-        NavigationStack(path: $pathStore.path) {
-            DetailView(number: 0, path: $pathStore.path)
-                .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i, path: $pathStore.path)
-                }
-        }
+//    @State private var pathStore = PathStore()
+//    
+//    
+//    var body: some View {
+//        
+//        NavigationStack(path: $pathStore.path) {
+//            DetailView(number: 0, path: $pathStore.path)
+//                .navigationDestination(for: Int.self) { i in
+//                    DetailView(number: i, path: $pathStore.path)
+//                }
+//        }
         
         //@Binding - lets you pass a piece of state into another view and modify it from there. Changing it in one place, will change it everywhere
 //        NavigationStack(path: $path) {
@@ -153,10 +194,10 @@ struct ContentView: View {
 //            }
 //        }
         
-    }
-}
+//    }
+//}
 
-//Day 42 - Hashable, navigation link and navigation destination
+//Day 43 - Hashable, navigation link and navigation destination
 
 //struct DetailView: View {
 //    var number: Int
